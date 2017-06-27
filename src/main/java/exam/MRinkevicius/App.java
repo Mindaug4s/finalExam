8e0339b8ff8d0d949c4c2576f99317105c699ac9
Mindaug4s <mindaugas.rinkevicius4@gmail.com>
2017-06-27 08:50:31 +0300

    First commit after configuring pom.xml dependencies

---
+++ src/main/java/exam/MRinkevicius/App.java
@@ -0,0 +1,13 @@
+package exam.MRinkevicius;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
